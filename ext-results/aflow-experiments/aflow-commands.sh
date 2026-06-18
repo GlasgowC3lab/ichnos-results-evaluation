9d8c7f33e06ce864d5fd47b7ec46dc3758bdae4b
westkath <2509480w@student.gla.ac.uk>
2026-06-18 15:46:22 +0100

    data

--- ext-results/aflow-experiments/aflow-commands.sh
+++ ext-results/aflow-experiments/aflow-commands.sh
@@ -4,9 +4,21 @@ python -m src.scripts.IchnosCF high_cpu_high_mem-1 1 schedutil_linear 5 1.0 0.39
 python -m src.scripts.IchnosCF high_cpu_high_mem-2 1 schedutil_linear 5 1.0 0.392 256 4
 python -m src.scripts.IchnosCF high_cpu_high_mem-3 1 schedutil_linear 5 1.0 0.392 256 4
 
+python -m src.scripts.IchnosCF high_cpu_med_mem-1 1 schedutil_linear 5 1.0 0.392 256 4
+python -m src.scripts.IchnosCF high_cpu_med_mem-2 1 schedutil_linear 5 1.0 0.392 256 4
+python -m src.scripts.IchnosCF high_cpu_med_mem-3 1 schedutil_linear 5 1.0 0.392 256 4
+
 python -m src.scripts.IchnosCF high_cpu_low_mem-1 1 schedutil_linear 5 1.0 0.392 256 4
 python -m src.scripts.IchnosCF high_cpu_low_mem-2 1 schedutil_linear 5 1.0 0.392 256 4
 python -m src.scripts.IchnosCF high_cpu_low_mem-3 1 schedutil_linear 5 1.0 0.392 256 4
+
+python -m src.scripts.IchnosCF med_cpu_high_mem-1 1 schedutil_linear 5 1.0 0.392 256 4
+python -m src.scripts.IchnosCF med_cpu_high_mem-2 1 schedutil_linear 5 1.0 0.392 256 4
+python -m src.scripts.IchnosCF med_cpu_high_mem-3 1 schedutil_linear 5 1.0 0.392 256 4
+
+python -m src.scripts.IchnosCF med_cpu_med_mem-1 1 schedutil_linear 5 1.0 0.392 256 4
+python -m src.scripts.IchnosCF med_cpu_med_mem-2 1 schedutil_linear 5 1.0 0.392 256 4
+python -m src.scripts.IchnosCF med_cpu_med_mem-3 1 schedutil_linear 5 1.0 0.392 256 4
 
 python -m src.scripts.IchnosCF low_cpu_high_mem-1 1 schedutil_linear 5 1.0 0.392 256 4
 python -m src.scripts.IchnosCF low_cpu_high_mem-2 1 schedutil_linear 5 1.0 0.392 256 4
--- ext-results/fixers/split-energy-log.sh
+++ ext-results/fixers/split-energy-log.sh
@@ -1,34 +1,43 @@
 # divide energy for 3 runs
 
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c40_pkg.txt temp/low_cpu_low_mem/1/stamp-1.csv c40 pkg
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c40_dram.txt temp/low_cpu_low_mem/1/stamp-1.csv c40 dram
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c42_pkg.txt temp/low_cpu_low_mem/1/stamp-1.csv c42 pkg
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c42_dram.txt temp/low_cpu_low_mem/1/stamp-1.csv c42 dram
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c44_pkg.txt temp/low_cpu_low_mem/1/stamp-1.csv c44 pkg
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c44_dram.txt temp/low_cpu_low_mem/1/stamp-1.csv c44 dram
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c45_pkg.txt temp/low_cpu_low_mem/1/stamp-1.csv c45 pkg
-python split-energy-log.py temp/low_cpu_low_mem/1/logs/c45_dram.txt temp/low_cpu_low_mem/1/stamp-1.csv c45 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c40_pkg.txt temp/high_cpu_low_mem/1/stamp-1.csv c40 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c40_dram.txt temp/high_cpu_low_mem/1/stamp-1.csv c40 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c42_pkg.txt temp/high_cpu_low_mem/1/stamp-1.csv c42 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c42_dram.txt temp/high_cpu_low_mem/1/stamp-1.csv c42 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c44_pkg.txt temp/high_cpu_low_mem/1/stamp-1.csv c44 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c44_dram.txt temp/high_cpu_low_mem/1/stamp-1.csv c44 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c45_pkg.txt temp/high_cpu_low_mem/1/stamp-1.csv c45 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/1/logs/c45_dram.txt temp/high_cpu_low_mem/1/stamp-1.csv c45 dram
 
-python get_wf_periods.py ../aflow-experiments/high_cpu_high_mem/1/trace
+python3 get_wf_periods.py temp/high_cpu_low_mem/1/trace
+mkdir temp/high_cpu_low_mem/1/energy/
+mv period_exports/* temp/high_cpu_low_mem/1/energy/
+mv run_1_*.csv temp/high_cpu_low_mem/1/energy/
 
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c40_pkg.txt temp/low_cpu_low_mem/2/stamp-2.csv c40 pkg
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c40_dram.txt temp/low_cpu_low_mem/2/stamp-2.csv c40 dram
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c42_pkg.txt temp/low_cpu_low_mem/2/stamp-2.csv c42 pkg
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c42_dram.txt temp/low_cpu_low_mem/2/stamp-2.csv c42 dram
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c44_pkg.txt temp/low_cpu_low_mem/2/stamp-2.csv c44 pkg
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c44_dram.txt temp/low_cpu_low_mem/2/stamp-2.csv c44 dram
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c45_pkg.txt temp/low_cpu_low_mem/2/stamp-2.csv c45 pkg
-python split-energy-log.py temp/low_cpu_low_mem/2/logs/c45_dram.txt temp/low_cpu_low_mem/2/stamp-2.csv c45 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c40_pkg.txt temp/high_cpu_low_mem/2/stamp-2.csv c40 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c40_dram.txt temp/high_cpu_low_mem/2/stamp-2.csv c40 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c42_pkg.txt temp/high_cpu_low_mem/2/stamp-2.csv c42 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c42_dram.txt temp/high_cpu_low_mem/2/stamp-2.csv c42 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c44_pkg.txt temp/high_cpu_low_mem/2/stamp-2.csv c44 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c44_dram.txt temp/high_cpu_low_mem/2/stamp-2.csv c44 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c45_pkg.txt temp/high_cpu_low_mem/2/stamp-2.csv c45 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/2/logs/c45_dram.txt temp/high_cpu_low_mem/2/stamp-2.csv c45 dram
 
-python get_wf_periods.py ../aflow-experiments/high_cpu_high_mem/2/trace
+python3 get_wf_periods.py temp/high_cpu_low_mem/2/trace
+mkdir temp/high_cpu_low_mem/2/energy/
+mv period_exports/* temp/high_cpu_low_mem/2/energy/
+mv run_2_*.csv temp/high_cpu_low_mem/2/energy/
 
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c40_pkg.txt temp/low_cpu_low_mem/3/stamp-3.csv c40 pkg
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c40_dram.txt temp/low_cpu_low_mem/3/stamp-3.csv c40 dram
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c42_pkg.txt temp/low_cpu_low_mem/3/stamp-3.csv c42 pkg
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c42_dram.txt temp/low_cpu_low_mem/3/stamp-3.csv c42 dram
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c44_pkg.txt temp/low_cpu_low_mem/3/stamp-3.csv c44 pkg
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c44_dram.txt temp/low_cpu_low_mem/3/stamp-3.csv c44 dram
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c45_pkg.txt temp/low_cpu_low_mem/3/stamp-3.csv c45 pkg
-python split-energy-log.py temp/low_cpu_low_mem/3/logs/c45_dram.txt temp/low_cpu_low_mem/3/stamp-3.csv c45 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c40_pkg.txt temp/high_cpu_low_mem/3/stamp-3.csv c40 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c40_dram.txt temp/high_cpu_low_mem/3/stamp-3.csv c40 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c42_pkg.txt temp/high_cpu_low_mem/3/stamp-3.csv c42 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c42_dram.txt temp/high_cpu_low_mem/3/stamp-3.csv c42 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c44_pkg.txt temp/high_cpu_low_mem/3/stamp-3.csv c44 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c44_dram.txt temp/high_cpu_low_mem/3/stamp-3.csv c44 dram
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c45_pkg.txt temp/high_cpu_low_mem/3/stamp-3.csv c45 pkg
+python3 split-energy-log.py temp/high_cpu_low_mem/3/logs/c45_dram.txt temp/high_cpu_low_mem/3/stamp-3.csv c45 dram
 
-python get_wf_periods.py ../aflow-experiments/high_cpu_high_mem/3/trace
+python3 get_wf_periods.py temp/high_cpu_low_mem/3/trace
+mkdir temp/high_cpu_low_mem/3/energy/
+mv period_exports/* temp/high_cpu_low_mem/3/energy/
+mv run_3_*.csv temp/high_cpu_low_mem/3/energy/
